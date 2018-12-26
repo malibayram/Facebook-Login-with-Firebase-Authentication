e0e8c3fedcc65f81a8119d0a6ba12717a937dca3
malibayram91 <malibayram91@gmail.com>
2018-12-26 08:28:59 +0300

    Twitter Login Added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -34,6 +35,33 @@ class _MyMainPageState extends State<MyMainPage> {
     return null;
   }
 
+  Future<FirebaseUser> _loginWithTwitter() async {
+    var twitterLogin = new TwitterLogin(
+      consumerKey: 'Y2My4EBwXnRLrlqATYeo9fYKX',
+      consumerSecret: 'xYXUhQ1KoHEIUMI1ZjH5RnnvPjFAJZLmQc7ZXecPiXoFQDvS3i',
+    );
+
+    final TwitterLoginResult result = await twitterLogin.authorize();
+
+    switch (result.status) {
+      case TwitterLoginStatus.loggedIn:
+        var session = result.session;
+        FirebaseUser user = await _auth.signInWithTwitter(
+            authToken: session.token, authTokenSecret: session.secret);
+        return user;
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+        debugPrint(result.status.toString());
+        return null;
+        break;
+      case TwitterLoginStatus.error:
+        debugPrint(result.errorMessage.toString());
+        return null;
+        break;
+    }
+    return null;
+  }
+
   void _logOut() async {
     await _auth.signOut().then((response) {
       isLogged = false;
@@ -43,6 +71,16 @@ class _MyMainPageState extends State<MyMainPage> {
 
   void _logIn() {
     _loginWithFacebook().then((response) {
+      if (response != null) {
+        myUser = response;
+        isLogged = true;
+        setState(() {});
+      }
+    });
+  }
+
+  void _logInTwitter() {
+    _loginWithTwitter().then((response) {
       if (response != null) {
         myUser = response;
         isLogged = true;
@@ -77,8 +115,17 @@ class _MyMainPageState extends State<MyMainPage> {
                   Image.network(myUser.photoUrl),
                 ],
               )
-            : FacebookSignInButton(
-                onPressed: _logIn,
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FacebookSignInButton(
+                    onPressed: _logIn,
+                  ),
+                  SizedBox(height: 30.0),
+                  TwitterSignInButton(
+                    onPressed: _logInTwitter,
+                  ),
+                ],
               ),
       ),
     );
